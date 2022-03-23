def7a2c338b263eec4d579d188e2b51f40c34857
Charles Phillips <51299296+CharlesPhillipsHouston@users.noreply.github.com>
2022-03-23 17:14:27 -0500

    Update main.cpp
    output with two different types of objects - strings and vectors. both can have spaces.

--- experiment.xcodeproj/main.cpp
+++ experiment.xcodeproj/main.cpp
@@ -2,7 +2,8 @@
 //  experiment trying to move output to a function, retaining pointer
 //  this does work, outputs strings to screen and creates output file
 //  this was fixed by kevin partin
-// now trying to output to a file - no joy yet
+//  now trying to output to a file - no joy yet. it creates but does not output
+//  also trying to read input from file, it also will not do it. grrrr
 //  Created by Charles Phillips on 3/16/22.
 
 #include <iostream>
@@ -13,21 +14,16 @@
 using namespace std;
 
 // The vector is passed a constant reference, which means the calling argument is passed as a reference, but the contents of the vector can not be changed.
-void writeOutput (const vector<string> &v, ostream &os=cout)
+void writeOutput1 (const vector<string> &v)
 {
-
-    for (string i : v) {
-        os << "Value: " << i << endl;
+    for (string i : v)
+    {
+        cout << "Value: " << i << endl;
     } //  end of for loop
 
-} // end of writeOutput function
+} // end of writeOutput1 function
 
-void writeOutput (const string obs[]) {
-
-}
-
-/*
-void writeOutput (string pointer[])  // replaces above line version for array of strings
+void writeOutput2 (string pointer[])  // replaces above line version for array of strings
 // this function does not return any values, etc
 
 {
@@ -42,40 +38,43 @@ void writeOutput (string pointer[])  // replaces above line version for array of
         pointer++;
     } //  end of for loop
 
-} // end of writeOutput function
-*/
+} // end of writeOutput2 function
 
 /*
-void readInput (string pointer[])  // replaces above line version for array of strings
+void readInput (stringin)  // replaces above line version for array of strings
 // this function does not return any values, etc
 
 {
     for (int i = 0; i < 4; i++)
     {
-        getline(stringin, observation)  ; // read in four lines
-        pointer++;
+        getline(stringin)  ; // read in four lines
+        
     } //  end of for loop
 
 } // end of readInput function
+*/
 
- */
+ int main(int argc, const char *argv[])
+{
+ //   FILE* spOutput;
+    
+  ofstream stringout("/Users/Charles/Desktop/common_files/string-out.txt");
+    // this does create string-out.txt in the correct directory
+    
+    ifstream stringin("/Users/Charles/Desktop/common_files/string-in.txt") ;
+    
+    string observation[] = {"a a", "b b", "c c", "d d"}; // should overwrite with values in file
+    // strings can have spaces in them
 
-int main(int argc, const char *argv[]) {
-
-    vector<string> v = {"Fred", "Wilma", "Barney", "Betty"};
-
-    writeOutput(v); // call function writeOutput
-
-    ofstream ofile("/Users/Charles/Desktop/common_files/string-out.txt");
-    ifstream ifile("/Users/Charles/Desktop/common_files/string-in.txt");
-
-    vector<string> observation = {"aaa", "bbb", "ccc", "ddd"}; // should overwrite with values in file
-
-    writeOutput(observation, ofile);  // call function writeOutput
-
-    ofile.close();
-    ifile.close();
-
+    vector<string> v = {"Fr ed", "Wil ma", "Bar ney", "Be tty"};
+    // vectors can have spaces in them
+    
+    writeOutput1(v);  // call function writeOutput
+    
+    writeOutput2(observation);  // call function writeOutput
+    stringout.close();
+    stringin.close();
     return 0;
 
 } // end of main
+
