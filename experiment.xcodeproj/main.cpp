fce4aaf033b617d4a04a17ff308a3d47d7bdf5b6
Charles Phillips <51299296+CharlesPhillipsHouston@users.noreply.github.com>
2022-04-09 16:55:05 -0500

    Update main.cpp
    reads from input file, writes to output file (from inside of main, not from writeoutput function).

--- experiment.xcodeproj/main.cpp
+++ experiment.xcodeproj/main.cpp
@@ -1,9 +1,7 @@
-//  main.cpp 28 mar 2022
+//  main.cpp 9 apr 2022
 //  experiment trying to output and input from a file, retaining pointer
-//  this does work, outputs strings to screen and creates output file
-//  this was fixed by kevin partin
-//  now does output to a file
-//  also trying to read input from file, it also will not do it. grrrr
+//  this has been worked on by kevin partin
+//  now does read from a file and write to an output file but not from a function
 //  Created by Charles Phillips on 3/16/22.
 
 #include <iostream>
@@ -13,29 +11,37 @@
 
 using namespace std;
 
+struct observations  // a structure called observations
+{
+    string observation;  // each string read in is an observation
+    int i;
+};
+
 // The vector is passed a constant reference, which means the calling argument is passed as a reference, but the contents of the vector can not be changed.
-void writeOutput1 (const vector<string> &v)  // this outputs names
+
+void writeOutput1 (const vector<string> &v)  // this outputs names to the screen
 {
     for (string i : v)
-        cout << "Value: " << i << endl;
-
+        cout << "writeoutput1: " << i << " Neanderthal? " << endl;
+  // just checking that you can put in other text for formatting, etc
 } // end of writeOutput1 function
 
-void writeOutput2 (string pointer[], ostream& os)  // replaces above line version for array of strings
-// this outputs the letters
+/*
+void writeOutput2 (string observation, stringout)  // replaces above line version for array of strings
+// this outputs the letters to a file
 //  this function does not return any values, etc
-
 {
     for (int i = 0; i < 4; i++)
     {
-        os << "Observation: " << *pointer << endl; // value of what is at pointer
+        os << "writeoutput2: " << *pointer << endl; // value of what is at pointer
         pointer++;
     } //  end of for loop
 
 } // end of writeOutput2 function
+*/
 
 /*
-void readInput (string observation)  // replaces above line version for array of strings
+void readInput (string observation&)  // replaces above line version for array of strings
 // this function does not return any values, etc
 
 {
@@ -51,25 +57,44 @@ void readInput (string observation)  // replaces above line version for array of
 int main(int argc, const char *argv[])
 {
     //   FILE* spOutput;
+    string observation;  // define a string called observation
+    int i;
 
     ofstream stringout("/Users/Charles/Desktop/common_files/string-out.txt");
     // this does create string-out.txt in the correct directory
 
     ifstream stringin("/Users/Charles/Desktop/common_files/string-in.txt");
+    
+    if (! stringin)
+    {
+        cout << "Error opening input file" << endl;
+    }
 
+    else
+        for (i =0; ! stringin.eof(); i++)
+        {
+            getline( stringin, observation);
+            cout << "main, if loop: " << observation << endl;
+            stringout << "main: " << observation << endl;
+            // this line does write to output file
+            cout << endl;
+    }
+    
     // This is a declaration for an array of strings.
-    string observation[] = {"a a", "b b", "c c", "d d"}; // should overwrite with values in file
-    // strings can have spaces in them
+  //  string observation[] = {"aaa", "bbb", "c c", "d d"}; // should overwrite with values in file
+    // observation is obviously one observation, there will be three total
 
     // This is a declaration for a vector of strings. Strings can have spaces and are declared using double quotes.
     vector<string> v = {"Fred", "Wilma", "Barney", "Betty"};
     // vectors can have spaces in them
-
-    writeOutput1(v); // call function writeOutput1
-
-    // You must pass the output stream to the output function.
-    writeOutput2(observation, stringout); // call function writeOutput2
+   // here trying to use (with mods!!) some code from the c++ site
     
+    writeOutput1(v); // call function writeOutput
+    // You must pass the output stream to the output function.
+    
+  // writeOutput2(observation, stringout);
+    
+    observations temp;
     
     stringout.close();
     stringin.close();
